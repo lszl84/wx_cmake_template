dce3e7eff790db6e9d2927309b23d1d95b7051a8
Lukasz <lukasz.sromek@gmail.com>
2022-10-22 11:56:27 +0200

    encoding and .git fixes for the copy script

--- copy_to_project.sh
+++ copy_to_project.sh
@@ -22,6 +22,6 @@ else
         SEDOPT=
     fi
 
-    SEDCMD="sed -i $SEDOPT -e \"s/wx_cmake_template/$2/g\" \$(find \"$1\" -type f)"
+    SEDCMD="LC_ALL=C sed -i $SEDOPT -e \"s/wx_cmake_template/$2/g\" \$(find \"$1\" -type f -not -path \"$1/.git\")"
     eval $SEDCMD
 fi
